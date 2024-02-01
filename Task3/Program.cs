7b0d5f7936d39b8b79980e68b582fbe62b777544
Denis Perevoshchikov <DobryDen>
2024-02-01 20:31:18 +0400

    Task 3 done!

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -4,7 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("введите первую фразу");
+        string FirstPhrase = Console.ReadLine();
+        Console.WriteLine("введите вторую фразу");
+        string SecondPhrase = Console.ReadLine();
+        Console.WriteLine("Ваша фраза: " + FirstPhrase + " " + SecondPhrase + "!");
     }
 }
 
