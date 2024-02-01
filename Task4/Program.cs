279625bd8af249d630a91312f0b1bd48db85ea73
Denis Perevoshchikov <DobryDen>
2024-02-01 20:43:29 +0400

    Task4 done!

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -4,7 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        const double Pi = 3.14;
+        double Radius = 5.0;
+        double Square = Pi * Radius * Radius;
+        Console.WriteLine("Площадь круга с радиусом " + Radius + " равна " + Square + "!");
     }
 }
 
