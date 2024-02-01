407f2f37831d533722ec69e8b3fe44098279e712
Denis Perevoshchikov <DobryDen>
2024-02-01 20:19:09 +0400

    Task2 done

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -4,7 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Input name");
+        string name = Console.ReadLine();
+        Console.WriteLine("Hello " + name + "!");
     }
 }
 
