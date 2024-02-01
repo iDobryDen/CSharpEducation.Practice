5960448c71ec7d6c839df616d8cf62ccc0423256
Denis Perevoshchikov <DobryDen>
2024-02-01 19:52:24 +0400

    Zadanie1

---
+++ Task0/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Task0;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        int age;
+        string Name;
+        string CompanyName;
+        bool pi;
+        double weight;
+    }
+}
+
