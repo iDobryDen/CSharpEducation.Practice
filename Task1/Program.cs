7a050900d2d4332f8749e5147c8afe25180faa19
Denis Perevoshchikov <DobryDen>
2024-02-01 20:04:13 +0400

    zadanie2

---
+++ Task1/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Task1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int age = 35;
+        string name = "Denis";
+        double temp = 24.0;
+        bool Women = true;
+    }
+}
+
