acedeb7f7da5ca68177e083badc2e73c8d37ff91
Denis Perevoshchikov <DobryDen>
2024-02-02 06:51:56 +0400

    Task 5a done!

--- Task5/Program.cs
+++ Task5/Program.cs
@@ -1,10 +1,15 @@
-﻿namespace Task5;
+﻿using System;
+
+namespace Task5;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        
+        double FirstLeg = 15.0;
+        double SecondLeg = 25.0;
+        double Hypotenuse = Math.Sqrt(FirstLeg * FirstLeg + SecondLeg * SecondLeg);
+        Console.WriteLine("Гипотенуза равна " + Hypotenuse);
     }
 }
-
