006c6aa0eeb9d123c093db01839e515b6e368bba
Denis Perevoshchikov <DobryDen>
2024-02-05 06:44:22 +0400

    Task5b done!

--- Task5b/Program.cs
+++ Task5b/Program.cs
@@ -4,7 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        double FirstLeg = 3.0;
+        double Hypotenuse = 5.0;
+        
+        double SecondLeg = Math.Sqrt(Hypotenuse * Hypotenuse - FirstLeg * FirstLeg);
+
+        Console.WriteLine("Второй катет равен " + SecondLeg);
     }
 }
 
