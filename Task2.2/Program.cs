f59716a80b9a80da8ac20880e486b1a9dad77961
Denis Perevoshchikov <DobryDen>
2024-02-08 19:54:41 +0400

    task2.2. done

--- Task2.2/Program.cs
+++ Task2.2/Program.cs
@@ -4,7 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("input integer from 1 to 5");
+        string num = Console.ReadLine();
+        int num2 = int.Parse(num);
+        if (num2 <= 0 || num2 >= 6)
+        {
+            Console.WriteLine("Ваше число не соответствует условиям задачи");
+        }
+        else if (num2 == 5)
+        {
+            Console.WriteLine("5 -  Отлично");
+        }
+        else if (num2 == 4)
+        {
+            Console.WriteLine("4 -  Хорошо");
+        }
+        else if (num2 == 3)
+        {
+            Console.WriteLine("3 -  Удовлетворительно");
+        }
+        else if (num2 == 2)
+        {
+            Console.WriteLine("2 -  Неудовлетворительно");
+        }
+        else if (num2 == 1)
+        {
+            Console.WriteLine("1 -  Ужасно");
+        }
     }
 }
 
