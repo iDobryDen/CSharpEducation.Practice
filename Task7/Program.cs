d6d876bf331119a4346dfbe8084608b03895a645
Denis Perevoshchikov <DobryDen>
2024-02-08 19:25:16 +0400

    Task7 done!

--- Task7/Program.cs
+++ Task7/Program.cs
@@ -4,7 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int num = 10;
+        int num2 = num % 2;
+        if (num2 == 0)
+        {
+            Console.WriteLine("Четное");
+        }
+        else
+            Console.WriteLine("Не четное");
     }
 }
 
