537ad33ae3fa95698fa27afe620c1b650d55c1cf
Denis Perevoshchikov <DobryDen>
2024-02-08 20:23:41 +0400

    task 2.3 done

--- Task2.3./Program.cs
+++ Task2.3./Program.cs
@@ -4,7 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int distanсe = 5000;
+        int km = distanсe / 1000;
+
+        Console.WriteLine(km);
+        int km2 = 7;
+        int сm = km2 * 100000;
+        Console.WriteLine(сm);
+
+        double speedmsec = 20;
+        double speedkmsec = speedmsec / 1000;
+        double speedkmhour = speedkmsec * 60 * 60;
+        Console.WriteLine(speedkmhour);
+
+        double C = 24;
+        double F = (C * 9 / 5) + 32;
+        Console.WriteLine(F);
     }
 }
 
