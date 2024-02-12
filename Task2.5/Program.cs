9c626ad9096c72685dea7ac6761961a85e4b1450
Denis Perevoshchikov <DobryDen>
2024-02-12 07:04:36 +0400

    task 2.5 a and c done.

--- Task2.5/Program.cs
+++ Task2.5/Program.cs
@@ -4,7 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        //a, c
+        int height = 4; // Задаем высоту треугольника
+        
+        for (int i = 1; i <= height; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write("&");
+            }
+            Console.WriteLine();
+        }
+
+        //b
+
     }
+
 }
+
 
