85980d107b0d42dfb9648da93906f993ae7e1b5e
Denis Perevoshchikov <DobryDen>
2024-02-13 06:41:41 +0400

    task2.6 done!

--- Task2.6/Program.cs
+++ Task2.6/Program.cs
@@ -4,7 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        //Создайте массив строк. В цикле заполните его значениями, которые вводит пользователь.
+        Console.WriteLine("Введите количество слов: ");
+        int amount = int.Parse(Console.ReadLine());
+        string[] words= new string[amount];
+        for (int i = 0; i < amount; i++)
+        {
+            Console.WriteLine("Введите слово c индексом: " + i);
+            words[i] = Console.ReadLine();
+        }
+
+        Console.WriteLine("Вы ввели :");
+        foreach(string word in words)
+        {
+            Console.WriteLine(word);
+        }
     }
 }
 
