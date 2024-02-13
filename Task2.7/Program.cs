f23d008743c70e12a8fd1f8e97ba6481e34d933b
Denis Perevoshchikov <DobryDen>
2024-02-13 07:08:45 +0400

    Task 2.7 done!

--- Task2.7/Program.cs
+++ Task2.7/Program.cs
@@ -4,7 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        //Создайте массив целых чисел. В цикле заполните его значениями счётчика в квадрате.
+        int[] nums = new int[] { 1, 2, 4 };
+        int num2 = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            nums[i] = nums[i] * nums[i];
+            Console.WriteLine(nums[i]);
+        }
+
+
+        foreach (int num in nums)
+        {
+            Console.WriteLine(num);
+        }
     }
 }
 
