ff205ed45e91a54606daecb2ffa63bb69defac0c
Denis Perevoshchikov <DobryDen>
2024-02-14 07:07:10 +0400

    Task2.8 done!

--- Task2.8/Program.cs
+++ Task2.8/Program.cs
@@ -4,7 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        //Создайте массив массивов. Размер каждого внутреннего массива должен быть на 1 больше, чем предыдущий. Первый массив должен быть размера 1.
+        Console.WriteLine("Введите желаемое количество подмассивов: ");
+        int a = int.Parse(Console.ReadLine());
+        int[][] matrix = new int[a][];
+
+        
+
+        for (int i = 0; i < a; i++)
+        {
+            matrix[i] = new int[i + 1];
+            for (int j = 0; j < matrix[i].Length; j++)
+            {
+                //matrix[i][j] = new Random().Next();
+                Console.WriteLine("Введите значение:");
+                matrix[i][j] = int.Parse(Console.ReadLine());
+            }
+        }
+
+        for (int i = 0; i < matrix.Length; i++)
+            for (int j = 0; j < matrix[i].Length; j++)
+                Console.WriteLine("Значения в массиве: " + matrix[i][j]);
     }
 }
 
