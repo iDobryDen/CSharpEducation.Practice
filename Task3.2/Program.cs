ff9d05fa7cba53cff095fdd178f9402456602645
Denis Perevoshchikov <DobryDen>
2024-02-18 12:34:05 +0400

    Task3.2 done!

--- Task3.2/Program.cs
+++ Task3.2/Program.cs
@@ -4,7 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        //Создать метод GetArray, который возвращает массив целых чисел и не принимает аргументов.
+        //Реализовывать метод не нужно, тело должно остаться пустым, но код должен компилироваться.
+        //В теле метода можно вернуть пустой массив. Метод вызовите в методе Main.
+
+        GetArray();
+    }
+    public static int[] GetArray()
+    {
+        return new int[0];
     }
 }
 
