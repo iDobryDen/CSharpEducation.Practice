581050b0874ba5e528e3225fd085036c0de7125c
Denis Perevoshchikov <DobryDen>
2024-02-18 12:48:57 +0400

    Task3.3 done!

--- Task3.3/Program.cs
+++ Task3.3/Program.cs
@@ -4,7 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+
+        //Создайте и реализуйте метод, который вычисляет сумму двух чисел и возвращает их сумму.
+        //Вызовите метод в методе Main и выведите результат на экран. Два числе передайте в метод как вам угодно.
+        //Метод, который вы напишите, не должен работать с консолью.
+        int c = 25;
+        int d = 10;
+        int z = GetArrow(c, d);
+        Console.WriteLine(z);
+    }
+    public static int GetArrow(int A , int B)
+    {
+        return A + B;
     }
 }
 
