2b47e679b5b0ad41f0e2fb15910abae7ba1ccf26
Denis Perevoshchikov <DobryDen>
2024-02-18 12:18:15 +0400

    Task3.1 done!

--- Task3.1/Program.cs
+++ Task3.1/Program.cs
@@ -4,7 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        //Создать метод GetName, который возвращает строку и не принимает аргументов. Реализовывать метод не нужно,
+        //тело должно остаться пустым, но код должен компилироваться. Можно использовать string.Empty для того, чтобы
+        //вернуть пустую строку. Метод вызовите в методе Main.
+        GetName();
+    }
+    public static string GetName()
+    {
+        return string.Empty;
     }
 }
 
