6f684e7073ee5e7854074b0a694e8d1f8a07c6c6
Denis Perevoshchikov <DobryDen>
2024-02-19 04:26:59 +0400

    Task3.6 done!

--- Task3.6/Program.cs
+++ Task3.6/Program.cs
@@ -4,7 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        //Создайте и реализуйте метод, который будет принимать число по ссылке (ref) и менять его знак на противоположный.
+        //Метод должен принимать один аргумент и не возвращать ничего.
+
+        int arr = int.Parse(Console.ReadLine());
+
+        Minus(ref arr);
+        Console.WriteLine(arr);
+    }
+    public static void Minus(ref int arr)
+    {
+        arr = -arr;
     }
 }
-
