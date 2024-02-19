06962312bac45109af6a4e1b6f15606fb4af4db2
Denis Perevoshchikov <DobryDen>
2024-02-19 04:53:45 +0400

    Task 3.7 done!

--- Task3.7/Program.cs
+++ Task3.7/Program.cs
@@ -4,7 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        //Создайте и реализуйте метод, который будет принимать два числа и менять их значения местами.Вызовите метод в Main.
+
+        int[] nums = new int[2];
+        for (int i = 0; i < nums.Length; i++)
+        {
+            Console.WriteLine("Введите число:");
+            nums[i] = int.Parse(Console.ReadLine());
+        }
+
+        Console.WriteLine("Исходный массив:");
+        PrintArray(nums);
+
+        ReversNums(nums);
+
+        Console.WriteLine("\nИнвертированный массив:");
+        PrintArray(nums);
+    }
+
+    public static void ReversNums(int[] args)
+    {
+        Array.Reverse(args);
+    }
+
+    public static void PrintArray(int[] args)
+    {
+        foreach (var item in args)
+        {
+            Console.Write(item + " ");
+        }
     }
 }
 
