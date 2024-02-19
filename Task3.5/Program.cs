8a43c1e1c6d559b409a391ffb93294c1fafa4088
Denis Perevoshchikov <DobryDen>
2024-02-19 04:08:59 +0400

    Task 3.5 done!

--- Task3.5/Program.cs
+++ Task3.5/Program.cs
@@ -4,7 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        //Создайте и реализуйте метод, который принимает на вход массив и инвертирует его.
+        //Вызовите его в методе Main. Исходный массив задайте сами.
+        //Инвертированный массив выведите на экран консоли.
+
+        int[] nums = new int[] { 1, 2, 4, 5 };
+
+        Console.WriteLine("Исходный массив:");
+        PrintArray(nums);
+
+        InvertNums(nums);
+
+        Console.WriteLine("\nИнвертированный массив:");
+        PrintArray(nums);
     }
+
+    public static void InvertNums(int[] arr)
+    {
+        Array.Reverse(arr);
+    }
+
+    static void PrintArray(int[] arr)
+    {
+        foreach (var item in arr)
+        {
+            Console.Write(item + " ");
+        }
+    }
+
 }
 
