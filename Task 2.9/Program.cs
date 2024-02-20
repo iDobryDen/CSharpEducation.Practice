4e25d58887f74a1e635186da671d7c90641a7397
Denis Perevoshchikov <DobryDen>
2024-02-20 04:29:16 +0400

    Task2.9 done!

--- Task 2.9/Program.cs
+++ Task 2.9/Program.cs
@@ -4,7 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        //Создайте двумерный массив. В циклах заполните его значениями счётчика. Выведите массив на экран.
+        int[,] matrix = new int[3, 3];
+
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                Console.WriteLine("Введите значение массива: ");
+                matrix[i, j] = int.Parse(Console.ReadLine());
+            }
+        }
+        Console.WriteLine("Получился массив:");
+        PrintArray(matrix);
+    }
+    public static void PrintArray(int[,] nums)
+    {
+        for (int i = 0; i < nums.GetLength(0); i++)
+        {
+            for (int j = 0; j < nums.GetLength(1); j++)
+            {
+                Console.Write(nums[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
     }
 }
-
