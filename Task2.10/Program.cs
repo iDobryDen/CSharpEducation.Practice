0393c2fc7aa2d9882aff5a4e8445e49d3ad6deae
Denis Perevoshchikov <DobryDen>
2024-02-20 04:47:53 +0400

    Task 2.10 done!

--- Task2.10/Program.cs
+++ Task2.10/Program.cs
@@ -4,7 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        //Напишите программу, которая проходит по массиву и находит наименьшее число.
+        int[] Numbers = new int[] { 25, 2, 10, 15, 43 };
+
+        int min = Numbers[0];
+        for (int i = 1; i < Numbers.Length; i++)
+        {
+            if (Numbers[i] < min)
+            {
+                min = Numbers[i];
+            }
+        }
+        Console.WriteLine("Наименьшее число в массиве: " + min);
     }
 }
 
