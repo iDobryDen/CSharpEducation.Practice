33ad73284ba9e87f7d8c6c7507981a9cea23fa3c
Denis Perevoshchikov <DobryDen>
2024-02-21 07:01:42 +0400

    Task3.5 done!

--- Task3.5/Program.cs
+++ Task3.5/Program.cs
@@ -8,7 +8,7 @@ class Program
         //Вызовите его в методе Main. Исходный массив задайте сами.
         //Инвертированный массив выведите на экран консоли.
 
-        int[] nums = new int[] { 1, 2, 4, 5 };
+        int[] nums = new int[] { 1, 2, 4, 5, 7 };
 
         Console.WriteLine("Исходный массив:");
         PrintArray(nums);
@@ -21,7 +21,15 @@ class Program
 
     public static void InvertNums(int[] arr)
     {
-        Array.Reverse(arr);
+        //Array.Reverse(arr);
+
+        for (int i = 0; i < arr.Length / 2; i++)
+        {
+            int temp = arr[i];
+            arr[i] = arr[arr.Length - 1 - i];
+            arr[arr.Length - 1 - i] = temp;
+        }
+
     }
 
     static void PrintArray(int[] arr)
